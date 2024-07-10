7264be584fb291a29be2f6721c968d133e552578
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-10 14:35:07 +0300

    feat: add ocelot api gateway

---
+++ Ocelot.WebHost/DI/ServicesConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Ocelot.DependencyInjection;
+
+namespace Ocelot.WebHost.DI;
+
+public static class ServicesConfiguration
+{
+    public static void ConfigureOcelot(this IServiceCollection services, IConfigurationManager configuration, IWebHostEnvironment environment)
+    {
+        configuration
+            .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"ocelot.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+        services.AddOcelot(configuration);
+    }
+
+    public static void AddAuth0Authentication(this IServiceCollection services, IConfiguration configuration) =>
+        services.AddAuthentication(
+            options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer("Auth0Key",
+                options =>
+                {
+                    options.Authority = $"https://{configuration["Auth0:Domain"]}/";
+                    options.Audience = configuration["Auth0:Audience"];
+                });
+}

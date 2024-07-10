e67e06674359545e30e4eae40cc14764f01e6b58
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-10 17:26:41 +0300

    feat: add models from services

---
+++ AdminPanelService/AdminPanel.BLL/Models/VehicleModel.cs
@@ -0,0 +1,20 @@
+﻿using AdminPanel.BLL.Enums;
+
+namespace AdminPanel.BLL.Models;
+
+public class VehicleModel
+{
+    public Guid Id { get; set; }
+
+    public string PlateNumber { get; set; } = string.Empty;
+    public int Odo { get; set; }
+    public decimal RentCost { get; set; }
+    public bool IsRented { get; set; }
+
+    public VehicleType VehicleType { get; set; }
+    public VehicleState VehicleState { get; set; }
+    public FuelType FuelType { get; set; }
+
+    public Guid ModelId { get; set; }
+    public CarModel? Model { get; set; }
+}

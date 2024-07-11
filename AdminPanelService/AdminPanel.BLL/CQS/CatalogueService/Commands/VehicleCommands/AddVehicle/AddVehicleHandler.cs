bf75849e4844e481ab0049ec1e9358b612a68dfa
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-11 14:40:19 +0300

    feat: add vehicle commands

---
+++ AdminPanelService/AdminPanel.BLL/CQS/CatalogueService/Commands/VehicleCommands/AddVehicle/AddVehicleHandler.cs
@@ -0,0 +1,12 @@
+﻿using AdminPanel.BLL.Models;
+using MediatR;
+
+namespace AdminPanel.BLL.CQS.CatalogueService.Commands.VehicleCommands.AddVehicle;
+
+public sealed class AddVehicleHandler : IRequestHandler<AddVehicleCommand, VehicleModel>
+{
+    public Task<VehicleModel> Handle(AddVehicleCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}

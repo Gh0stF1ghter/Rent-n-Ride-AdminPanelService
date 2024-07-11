9ef739e36753c2ece1220fdeb4ba41a9cf46f159
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-11 14:39:53 +0300

    feat: add carMOdel commands

---
+++ AdminPanelService/AdminPanel.BLL/CQS/CatalogueService/Commands/CarModelCommands/AddCarModel/AddCarModelHandler.cs
@@ -0,0 +1,12 @@
+﻿using AdminPanel.BLL.Models;
+using MediatR;
+
+namespace AdminPanel.BLL.CQS.CatalogueService.Commands.CarModelCommands.AddCarModel;
+
+public sealed class AddCarModelHandler : IRequestHandler<AddCarModelCommand, CarModel>
+{
+    public Task<CarModel> Handle(AddCarModelCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AdminPanelService/AdminPanel.BLL/CQS/CatalogueService/Commands/CarModelCommands/DeleteCarModel/DeleteCarModelHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace AdminPanel.BLL.CQS.CatalogueService.Commands.CarModelCommands.DeleteCarModel;
+
+public sealed class DeleteCarModelHandler : IRequestHandler<DeleteCarModelCommand>
+{
+    public Task Handle(DeleteCarModelCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}

b1ccb1952fd5b47afe49f17d4db94db950da7ee2
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-15 13:46:00 +0300

    feat: add controllers

---
+++ AdminPanelService/AdminPanel.API/Controllers/ManufacturerController.cs
@@ -0,0 +1,68 @@
+﻿using AdminPanel.BLL.CQS.CatalogueService.Commands.ManufacturerCommands.AddManufacturer;
+using AdminPanel.BLL.CQS.CatalogueService.Commands.ManufacturerCommands.DeleteManufacturer;
+using AdminPanel.BLL.CQS.CatalogueService.Commands.ManufacturerCommands.UpdateManufacturer;
+using AdminPanel.BLL.CQS.CatalogueService.Queries.ManufacturerQueries.GetManufacturerById;
+using AdminPanel.BLL.CQS.CatalogueService.Queries.ManufacturerQueries.GetManufacturersInRange;
+
+namespace AdminPanel.API.Controllers;
+
+[ApiController]
+[Route("api/manufacturer")]
+public class ManufacturerController(ISender sender) : ControllerBase
+{
+    [HttpGet]
+    [ActionName("GetAllManufacturersInRange")]
+    public async Task<IEnumerable<ManufacturerViewModel>> GetAll([FromQuery] int page, [FromQuery] int pageSize, CancellationToken cancellationToken)
+    {
+        var manufacturers = await sender.Send(new GetManufacturersInRangeQuery(page, pageSize), cancellationToken);
+
+        var manufacturersVMs = manufacturers.Adapt<IEnumerable<ManufacturerViewModel>>();
+
+        return manufacturersVMs;
+    }
+
+    [HttpGet("{id}")]
+    [ActionName("GetManufacturerById")]
+    public async Task<ManufacturerViewModel> GetById([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var manufacturer = await sender.Send(new GetManufacturerByIdQuery(id), cancellationToken);
+
+        var manufacturerVM = manufacturer.Adapt<ManufacturerViewModel>();
+
+        return manufacturerVM;
+    }
+
+    [HttpPost]
+    [ActionName("CreateManufacturer")]
+    public async Task<ManufacturerViewModel> Create([FromBody] ShortManufacturerViewModel createManufacturerViewModel, CancellationToken cancellationToken)
+    {
+        var manufacturerModel = createManufacturerViewModel.Adapt<ManufacturerModel>();
+
+        var newManufacturer = await sender.Send(new AddManufacturerCommand(manufacturerModel), cancellationToken);
+
+        var manufacturerVM = newManufacturer.Adapt<ManufacturerViewModel>();
+
+        return manufacturerVM;
+    }
+
+    [HttpPut("{id}")]
+    [ActionName("UpdateManufacturerById")]
+    public async Task<ManufacturerViewModel> Update([FromRoute] Guid id, [FromBody] ShortManufacturerViewModel updateManufacturerViewModel, CancellationToken cancellationToken)
+    {
+        var manufacturerModel = updateManufacturerViewModel.Adapt<ManufacturerModel>();
+        manufacturerModel.Id = id;
+
+        var newManufacturer = await sender.Send(new UpdateManufacturerCommand(manufacturerModel), cancellationToken);
+
+        var manufacturerVM = newManufacturer.Adapt<ManufacturerViewModel>();
+
+        return manufacturerVM;
+    }
+
+    [HttpDelete("{id}")]
+    [ActionName("DeleteManufacturerById")]
+    public async Task Delete([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        await sender.Send(new DeleteManufacturerCommand(id), cancellationToken);
+    }
+}
---
+++ AdminPanelService/AdminPanel.API/Controllers/VehicleController.cs
@@ -0,0 +1,68 @@
+﻿using AdminPanel.BLL.CQS.CatalogueService.Commands.VehicleCommands.AddVehicle;
+using AdminPanel.BLL.CQS.CatalogueService.Commands.VehicleCommands.DeleteVehicle;
+using AdminPanel.BLL.CQS.CatalogueService.Commands.VehicleCommands.UpdateVehicle;
+using AdminPanel.BLL.CQS.CatalogueService.Queries.VehicleQueries.GetVehicleById;
+using AdminPanel.BLL.CQS.CatalogueService.Queries.VehicleQueries.GetVehiclesInRange;
+
+namespace AdminPanel.API.Controllers;
+
+[ApiController]
+[Route("api/vehicle")]
+public class VehicleController(ISender sender) : ControllerBase
+{
+    [HttpGet]
+    [ActionName("GetAllVehiclesInRange")]
+    public async Task<IEnumerable<VehicleViewModel>> GetAll([FromQuery] int page, [FromQuery] int pageSize, CancellationToken cancellationToken)
+    {
+        var vehicles = await sender.Send(new GetVehiclesInRangeQuery(page, pageSize), cancellationToken);
+
+        var vehiclesVMs = vehicles.Adapt<IEnumerable<VehicleViewModel>>();
+
+        return vehiclesVMs;
+    }
+
+    [HttpGet("{id}")]
+    [ActionName("GetVehicleById")]
+    public async Task<VehicleViewModel> GetById([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var vehicle = await sender.Send(new GetVehicleByIdQuery(id), cancellationToken);
+
+        var vehicleVM = vehicle.Adapt<VehicleViewModel>();
+
+        return vehicleVM;
+    }
+
+    [HttpPost]
+    [ActionName("CreateVehicle")]
+    public async Task<VehicleViewModel> Create([FromBody] ShortVehicleViewModel createClientViewModel, CancellationToken cancellationToken)
+    {
+        var createVehicleModel = createClientViewModel.Adapt<VehicleModel>();
+
+        var newVehicle = await sender.Send(new AddVehicleCommand(createVehicleModel), cancellationToken);
+
+        var vehicleVM = newVehicle.Adapt<VehicleViewModel>();
+
+        return vehicleVM;
+    }
+
+    [HttpPut("{id}")]
+    [ActionName("UpdateVehicleById")]
+    public async Task<VehicleViewModel> Update([FromRoute] Guid id, [FromBody] ShortVehicleViewModel updateVehicleViewModel, CancellationToken cancellationToken)
+    {
+        var vehicleModel = updateVehicleViewModel.Adapt<VehicleModel>();
+        vehicleModel.Id = id;
+
+        var newVehicle = await sender.Send(new UpdateVehicleCommand(vehicleModel), cancellationToken);
+
+        var vehicleVM = newVehicle.Adapt<VehicleViewModel>();
+
+        return vehicleVM;
+    }
+
+    [HttpDelete("{id}")]
+    [ActionName("DeleteVehicleById")]
+    public async Task Delete([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        await sender.Send(new DeleteVehicleCommand(id), cancellationToken);
+    }
+}

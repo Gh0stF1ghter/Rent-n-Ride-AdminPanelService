894efa3aa0fb19cdc3f169c07a37b4129c8c7483
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-16 10:44:05 +0300

    feat: add massTransit

--- AdminPanelService/AdminPanel.BLL/DI/ServicesConfiguration.cs
+++ AdminPanelService/AdminPanel.BLL/DI/ServicesConfiguration.cs
@@ -1,14 +1,29 @@
 ﻿using Mapster;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace AdminPanel.BLL.DI;
 public static class ServicesConfiguration
 {
-    public static void AddApplicationDependencies(this IServiceCollection services)
+    public static void AddApplicationDependencies(this IServiceCollection services, IConfiguration configuration)
     {
         TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
 
+        services.AddMessageBroker(configuration);
+
         services.AddMediatR(_ => _.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
     }
+
+    public static void AddMessageBroker(this IServiceCollection services, IConfiguration configuration) =>
+        services.AddMassTransit(cfg =>
+        {
+            cfg.SetKebabCaseEndpointNameFormatter();
+
+            cfg.UsingRabbitMq((context, factoryCfg) =>
+            {
+                factoryCfg.Host(configuration.GetConnectionString("RabbitMQ"));
+            });
+        });
 }

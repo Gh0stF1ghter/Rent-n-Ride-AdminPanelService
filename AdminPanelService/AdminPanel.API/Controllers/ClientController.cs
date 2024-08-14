55c42f7aff50109af2ebd4f6171a3d1012865dcf
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-08-14 17:59:42 +0300

    fix: call carModel mediator from client controller

--- AdminPanelService/AdminPanel.API/Controllers/ClientController.cs
+++ AdminPanelService/AdminPanel.API/Controllers/ClientController.cs
@@ -1,14 +1,15 @@
-﻿using AdminPanel.BLL.CQS.CatalogueService.Queries.CarModelQueries.GetCarModelById;
+﻿using AdminPanel.API.ViewModels.UpdateViewModels;
+using AdminPanel.BLL.CQS.CatalogueService.Queries.CarModelQueries.GetCarModelById;
 using AdminPanel.BLL.CQS.UserService.ClientCommands.AddClient;
 using AdminPanel.BLL.CQS.UserService.ClientCommands.DeleteClient;
 using AdminPanel.BLL.CQS.UserService.ClientCommands.UpdateClient;
+using AdminPanel.BLL.CQS.UserService.ClientQueries.GetVehicleById;
 using AdminPanel.BLL.CQS.UserService.ClientQueries.GetVehiclesInRange;
 using Microsoft.AspNetCore.Authorization;
 
 namespace AdminPanel.API.Controllers;
 
 [ApiController]
-[Authorize("use:dashboard")]
 [Route("api/client")]
 public class ClientController(ISender sender) : ControllerBase
 {
@@ -27,7 +28,7 @@ public class ClientController(ISender sender) : ControllerBase
     [ActionName("GetClientById")]
     public async Task<ClientViewModel> GetById([FromRoute] Guid id, CancellationToken cancellationToken)
     {
-        var client = await sender.Send(new GetCarModelByIdQuery(id), cancellationToken);
+        var client = await sender.Send(new GetClientByIdQuery(id), cancellationToken);
 
         var clientVM = client.Adapt<ClientViewModel>();
 
@@ -45,7 +46,7 @@ public class ClientController(ISender sender) : ControllerBase
 
     [HttpPut("{id}")]
     [ActionName("UpdateClientById")]
-    public async Task Update([FromRoute] Guid id, [FromBody] ShortClientViewModel updateClientViewModel, CancellationToken cancellationToken)
+    public async Task Update([FromRoute] Guid id, [FromBody] UpdateClientViewModel updateClientViewModel, CancellationToken cancellationToken)
     {
         var clientModel = updateClientViewModel.Adapt<ClientModel>();
 

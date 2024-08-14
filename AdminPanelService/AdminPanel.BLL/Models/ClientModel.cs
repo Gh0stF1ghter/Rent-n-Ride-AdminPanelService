f969ead2d9574e72aec2b05f9905c5b97556d5bb
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-08-14 18:00:16 +0300

    feat: add email & password validator

--- AdminPanelService/AdminPanel.BLL/Models/ClientModel.cs
+++ AdminPanelService/AdminPanel.BLL/Models/ClientModel.cs
@@ -4,6 +4,7 @@ public class ClientModel
 {
     public Guid Id { get; set; }
 
+    public string Password { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
